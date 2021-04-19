4935c1916fde56ae0be835e961d2976e14b94ce9
Matthew <matsgeco92@hotmail.it>
2021-04-19 19:00:12 +0200

    Milestone 1 e 2

--- js/script.js
+++ js/script.js
@@ -2,24 +2,34 @@ function initVue() {
   new Vue ({
     el:".container",
     data: {
-      array:[]
+      array:[],
+      value: "",
+      flags: {
+        flagEn: "img/Inglese.png",
+        flagIt: "img/Italia.jpg"
+      }
     },
-    mounted() {
-      axios.get("",{
-        params: {
-
-        }
-      })
-      .then(data => {
-        console.log(data);
-      })
-      .catch(() => console.log('error'));
-    }
+    methods:{
+      searchMovie: function () {
+        axios.get("https://api.themoviedb.org/3/search/movie",{
+          params: {
+            'api_key': '1550e050d06eb77cc575c30c39bfab7c',
+            'query' : this.value
+          }
+        })
+        .then(data => {
+          // console.log(data);
+          this.array= data.data.results;
+          console.log(this.array);
+        })
+        .catch(() => console.log('error'));
+      }
+    },
   });
 }
 
 function init() {
-
+  initVue();
 
 }
 
