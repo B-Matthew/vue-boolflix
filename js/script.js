b29e62bbb7cf5cb6bf1a73a58462aaa6e62ae1df
Matthew <matsgeco92@hotmail.it>
2021-04-20 00:12:36 +0200

    Milestone 2

--- js/script.js
+++ js/script.js
@@ -1,14 +1,22 @@
 function initVue() {
   new Vue ({
     el:".container",
+
     data: {
-      array:[],
+      movies:[],
+      tvSeries: [],
       value: "",
       flags: {
         flagEn: "img/Inglese.png",
         flagIt: "img/Italia.jpg"
-      }
+      },
+      imgForm:{
+        imgPath: "https://image.tmdb.org/t/p/",
+        imgSize:"w342",
     },
+  },
+
+
     methods:{
       searchMovie: function () {
         axios.get("https://api.themoviedb.org/3/search/movie",{
@@ -19,18 +27,34 @@ function initVue() {
         })
         .then(data => {
           // console.log(data);
-          this.array= data.data.results;
-          console.log(this.array);
+          this.movies= data.data.results;
+          console.log(this.movies);
+        });
+
+        axios.get("https://api.themoviedb.org/3/search/tv",{
+          params: {
+            'api_key':'1550e050d06eb77cc575c30c39bfab7c',
+            'query': this.value
+          }
         })
-        .catch(() => console.log('error'));
-      }
+        .then(data => {
+          this.tvSeries = data.data.results;
+          console.log(this.tvSeries);
+        });
+      },
+      // getVoteInteger: function (){
+      //   for (var i = 0; i < this.movies.length; i++) {
+      //     const item = this.movies[i];
+      //     Math.ceil(item.vote_average);
+      //   }
+      // }
     },
   });
 }
 
 function init() {
   initVue();
-
 }
+
 
 $(document).ready(init);
